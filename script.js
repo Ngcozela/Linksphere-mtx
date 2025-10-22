adfcc67de3f9c01d3215d8521c012172e24c5a80
Ngcozela <ngcozelauyolo@gmail.com>
2025-10-22 14:45:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function openCollection(num) {
+  alert("Opening Collection " + num + "...");
+  // In the future, you could redirect to another page:
+  // window.location.href = `collection${num}.html`;
+}
